a7c33ba7fa42875201374f02a02b855996975ab1
jasonboru <jasonboru@gmail.com>
2017-04-09 13:33:31 -0400

    Initial files some psuedo coding

---
+++ flashcards.js
@@ -0,0 +1,22 @@
+//initially give option to the user to Create new flashcards or use exiting ones.
+
+//If the choice is to create a card then ask what type 'basic or cloze'
+
+		// run a createCard function that runs a BasicCard Constructor or ClozeCard constructor based on the users answer.
+
+			// **Basic Card Constructor -- ask the user for there question and then ask for an answer. Store the responses in 
+			//   a JSON object to pull from later
+
+			// **Cloze Card Constuctor -- ask the user for the full text of their card. Then ask for the cloze portion they
+			//   want to replace with '...'
+
+			// After a card is created ask the user if they would like to make another card (recall createCard Function), use the
+			// card (call the askQuestions function), or exit the application back to the command prompt.
+
+//if the answer is 'Use' then run an askQuestions function
+
+			// pull the stored cards from the stored JSON and present them to the user. use a function that uses a counter to loop 
+			// through the questions using recursion.
+
+			//after each question print if the user is correct, if not print the correct answer.
+
